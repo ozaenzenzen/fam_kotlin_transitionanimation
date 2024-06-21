f1f890094b760c8c591a2d9708e06da6b77a8977
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-21 17:04:11 +0700

    update options compat

--- app/src/main/java/com/dicoding/picodiploma/myrecyclerview/adapter/ListHeroAdapter.kt
+++ app/src/main/java/com/dicoding/picodiploma/myrecyclerview/adapter/ListHeroAdapter.kt
@@ -50,8 +50,17 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
             itemView.setOnClickListener {
                 val intent = Intent(itemView.context, HeroActivity::class.java)
                 intent.putExtra("Hero", hero)
-                itemView.context.startActivity(intent,
-                    ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity)
+
+                val optionsCompat: ActivityOptionsCompat =
+                    ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        itemView.context as Activity,
+                        Pair(imgPhoto, "profile"),
+                        Pair(tvName, "name"),
+                        Pair(tvDescription, "description"),
+                    )
+                itemView.context.startActivity(
+                    intent,
+                    optionsCompat
                         .toBundle()
                 )
             }
